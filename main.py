5440d1ea3c7e0dd11b94949dbaa8eedd9e9b0731
ggm77 <shmshm86@naver.com>
2023-05-15 23:42:40 +0900

    Add main page and login page.

--- main.py
+++ main.py
@@ -23,12 +23,24 @@ templets = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse) #main page
 def index(request:Request):
     return templets.TemplateResponse("", {"request":request})
 
 @app.get("/login", response_class=HTMLResponse)
 def loginIndex(request:Request):
+    return templets.TemplateResponse("", {"request":request})
+
+@app.post("/", response_class=HTMLResponse) #logined main page
+def login_index(request:Request, id:str=Form(...)):
+    return templets.TemplateResponse("", {"request":request, "id":id})
+
+
+
+
+#404 page
+@app.get("/{item_id}", response_class=HTMLResponse)
+def error(request:Request):
     return templets.TemplateResponse("", {"request":request})
 
 
